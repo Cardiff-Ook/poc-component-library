d6acf8af6f80597f2fdc43264b923c65f61e6ad9
Jason Manly <evening-gazebo.0d@icloud.com>
2022-11-04 15:31:59 +0000

    Adding storybook stories

---
+++ src/components/page-layout/page-header-bar/page-header-bar.stories.js
@@ -0,0 +1,10 @@
+import readme from './readme.md';
+
+export default {
+    title: 'page layout/page-header-bar',
+    parameters: {
+      notes: readme
+    }
+  };
+
+  export const Default = () => `<page-header-bar name="Jeff"></page-header-bar>`;
---
+++ src/components/page-layout/text-title-bar/text-title-bar.stories.js
@@ -0,0 +1,10 @@
+import readme from './readme.md';
+
+export default {
+    title: 'page layout/text-title-bar',
+    parameters: {
+      notes: readme
+    }
+  };
+
+  export const Default = () => `<text-title-bar>For Car, Home and Van customers, login below to manage your cover</text-title-bar-bar>`;
