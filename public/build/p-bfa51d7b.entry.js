276eb26a1284577ba2d9b6454d9f7c0047c5095f
Jason Manly <evening-gazebo.0d@icloud.com>
2022-11-04 09:28:07 +0000

    Fixing image and security issues

--- public/build/p-bfa51d7b.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as t}from"./p-4abcca95.js";const i=class{constructor(t){s(this,t),this.disabled=!1,this.options=[]}render(){return t("div",null,t("label",{htmlFor:this.name},this.label),t("select",{name:this.name,id:this.name,disabled:this.disabled},this.options.map((s=>t("option",{value:s.value},s.displayText)))))}};i.style=":host{display:block}";export{i as my_component}
---
+++ public/build/policy-subsection-header.entry.js
@@ -0,0 +1,15 @@
+import { r as registerInstance, h } from './index-bbb3174a.js';
+
+const policySubsectionHeaderCss = ":host{display:block}.nobg{background-color:var(--gw-grey-light-63-color) !important}h1{width:512px}.subsectionText{margin-top:0;text-align:left;margin-bottom:24px;color:#4A4643!important;font-size:20px;font-style:normal;font-family:jaf-facitweb, Helvetica, Arial;font-weight:800;line-height:24px;word-wrap:break-word}";
+
+const PolicySubsectionHeader = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return (h("div", { class: "jut__Grid__grid jut__Grid__vgapMedium jut__Grid__hgapNone PolicyListComponent_policyContainer__23NJ9 nobg", id: "policyContainer", "data-testid": "policyListComponent" }, h("div", { class: "jut__Grid__grid jut__Grid__vgapMedium jut__Grid__hgapSmall PolicyListComponent_policyDetails__GSIKK", id: "policyDetails" }, h("h1", { class: "subsection", title: "" }, h("div", { class: "markdownWrapper" }, h("span", { class: "markdownParagraph" }, h("span", { class: "subsectionText" }, h("slot", null))))))));
+  }
+};
+PolicySubsectionHeader.style = policySubsectionHeaderCss;
+
+export { PolicySubsectionHeader as policy_subsection_header };
