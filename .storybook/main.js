b5fefc15b62a85871e4a500971295d8b587d21ec
Jason Manly <evening-gazebo.0d@icloud.com>
2022-11-08 12:47:25 +0000

    Controls sort of working now

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-notes/register"
+    "@storybook/addon-notes/register",
+    "@storybook/addon-knobs"
   ],
   "framework": "@storybook/html",
   staticDirs: ['../dist', '../public/assets'],
--- src/components/page-layout/page-header-bar/page-header-bar.stories.js
+++ src/components/page-layout/page-header-bar/page-header-bar.stories.js
@@ -1,10 +1,33 @@
 import readme from './readme.md';
+import PageHeaderBar from '../../../../dist/collection/components/page-layout/page-header-bar/page-header-bar';
+import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
     title: 'page layout/page-header-bar',
+    component: PageHeaderBar,
+    argTypes: {
+      name: {
+        control: 'text',
+        description: 'The name to be displayed',
+        table: {
+          category: 'content'
+        }
+      },
+    },
     parameters: {
       notes: readme
     }
   };
 
-  export const Default = () => `<page-header-bar name="Jeff"></page-header-bar>`;
+  const defaultArgs = {
+    name: 'Biff',
+  };
+
+  const Template = args => {
+    return <page-header-bar {...defaultArgs}></page-header-bar>;
+  };
+
+
+  export const Default = Template.bind({});
+  Default.args = {...defaultArgs};
