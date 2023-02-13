f2119d412190fd63ddf874cb6663cd93475c4fe4
Jason Manly <evening-gazebo.0d@icloud.com>
2023-02-13 17:16:15 +0000

    updated stylings

---
+++ public/build/p-9aab14a2.system.entry.js
@@ -0,0 +1,1 @@
+System.register(["./p-96f6b329.system.js"],(function(t){"use strict";var n,e,r;return{setters:[function(t){n=t.r;e=t.h;r=t.H}],execute:function(){var u=":host{-ms-flex:1 1 auto;flex:1 1 auto}";var o=t("new_nav_bar_logo",function(){function t(t){n(this,t)}t.prototype.render=function(){return e(r,null,e("ng-content",null,e("slot",null)))};return t}());o.style=u}}}));
