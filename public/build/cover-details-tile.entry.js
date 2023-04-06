f30cba5ab7d2d453a790add47bfb7c67d72218f8
Jason Manly <evening-gazebo.0d@icloud.com>
2023-04-06 15:15:17 +0100

    Fixed nav bar css issues

---
+++ public/build/cover-details-tile.entry.js
@@ -0,0 +1,49 @@
+import { r as registerInstance, h, g as getElement } from './index-cd495fd3.js';
+
+const coverDetailsTileCss = ":host{display:block}";
+
+const CoverDetailsTile = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  arrayDataWatcher(newValue) {
+    if (typeof newValue === 'string') {
+      this._coverDetails = JSON.parse(newValue);
+    }
+    else {
+      this._coverDetails = newValue;
+    }
+    if (this._coverDetails !== undefined)
+      this._coverDetails.imageSrc = `/assets/claim-cover-${this._coverDetails.policytype}.svg`;
+  }
+  toggle() {
+    var btn = this.element.querySelectorAll('button');
+    this.collapsed = !this.collapsed;
+    if (!!btn[0])
+      if (this.collapsed) {
+        btn[0].innerHTML = '<span id="selectedSpan"><span>View Summary</span></span>';
+        btn[0].classList.remove("SelectButton_selectButton__2EnMv");
+        btn[0].classList.remove("jut__Button__materialIcon");
+      }
+      else {
+        btn[0].innerHTML = '<span id="selectedSpan"><span>Selected</span><i class="mir mi-done jut__Button__icon jut__Button__rightIcon"></i></span>';
+        btn[0].classList.add("SelectButton_selectButton__2EnMv");
+        btn[0].classList.add("jut__Button__materialIcon");
+      }
+  }
+  componentWillLoad() {
+    this.arrayDataWatcher(this.coverdetails);
+    this.collapsed = true;
+    this._tileId = `riskcoverList${this.coverindex}`;
+  }
+  render() {
+    return (h("div", { id: this._tileId }, h("div", null, h("div", { class: "jut__Grid__grid jut__Grid__vgapMedium jut__Grid__hgapMedium CoverList_coverContainerDiv__3qiY1", id: "coverContainerDiv" }, h("div", { id: "detailsContainerDiv" }, h("div", { id: "detailsAndIconContainer", class: "CoverList_detailsAndIconContainer__1UIZA" }, h("div", { id: "coverIconLabelDiv", class: "CoverList_coverIconLabelDiv__v-dgX" }, h("img", { src: "/assets/claim-cover-car.svg", id: "coverIconImg", class: "CoverList_coverIconImg__2T5IO" })), h("div", { id: "coverDetails", class: "CoverList_coverDetails__6uRwg" }, h("div", { id: "coverLabelDiv", class: "CoverList_coverLabelDiv__R-tnB" }, h("div", { id: "coverTitleContainer", class: "" }, h("span", { id: "coverLabel" }, "COVER"), h("span", { id: "statusWithDate" }, h("span", { id: "coverStatusText", class: "CoverList_coverStatusText__1iTUz" }), h("span", { id: "coverStatusDate" })))), h("div", { id: "coverDetailDiv", class: "CoverList_coverDetailDiv__2a8_A" }, h("span", { id: "coverDetail", class: "CoverList_coverDetailText__1uT1F", "data-cs-mask": "" }, this._coverDetails.coverList[0].vehicleData.vehicleMake, " ", this._coverDetails.coverList[0].vehicleData.vehicleModel)), h("div", { id: "coverRegPostCodeDiv", class: "CoverList_coverRegPostCodeDiv__1RZPc" }, h("span", { id: "coverRegPostCode", class: "CoverList_coverRegPostCodeText__LkqLE", "data-cs-mask": "" }, "Reg No: ", this._coverDetails.coverList[0].vehicleData.vehicleReg))))), h("button", { type: "button", class: "digitalButton__button jut__Button__button digitalButton__secondary jut__Button__secondary", id: "viewButton", "data-testid": "viewButton", onClick: this.toggle.bind(this) }, h("span", { id: "selectedSpan" }, h("span", null, "View Summary"))))), h("div", { id: "content", hidden: this.collapsed }, h("div", { id: "riskTitleContainer", class: "RiskCoverDetails_riskTitleContainer__2FfQl" }, h("h3", { id: "riskTitle", class: "RiskCoverDetails_title__30skA" }, "Your Cover Details")), h("div", { id: "riskCoverDetailsContainer", class: "RiskCoverDetails_riskCoverDetailsContainer__2_wY7 RiskCoverDetails_sectionContainer___g21v" }, h("details-container", { detailstype: "policy", coverdetails: this.coverdetails }), h("details-container", { detailstype: "car", coverdetails: this.coverdetails })))));
+  }
+  get element() { return getElement(this); }
+  static get watchers() { return {
+    "coverdetails": ["arrayDataWatcher"]
+  }; }
+};
+CoverDetailsTile.style = coverDetailsTileCss;
+
+export { CoverDetailsTile as cover_details_tile };
---
+++ public/build/login-form.entry.js
@@ -0,0 +1,42 @@
+import { r as registerInstance, f as createEvent, h, g as getElement } from './index-cd495fd3.js';
+
+const loginFormCss = "#loginFormContainer{display:grid;grid-template-columns:auto}.loginForm{grid-row:1;margin-bottom:16px;display:block;margin-top:0em}.inputFieldContainer{width:100%;border:0;margin:0;display:inline-flex;padding:0;position:relative;min-width:0;margin-bottom:32px;flex-direction:column;vertical-align:top}.inputFieldContainer>label{transition:color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms,transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;transform:translate(0, 1.5px) scale(0.75);transform-origin:top left;top:0;left:0;position:relative;transform:translate(0, 24px) scale(1);color:#4A4643;display:block;font-size:14px;transform:none !important;font-style:normal;transition:none !important;font-family:jaf-facitweb, Helvetica, Arial;font-weight:normal;line-height:24px;padding:0}.inputFieldContainer>span{color:#4A4643;display:block;font-size:14px;transform:none !important;font-style:normal;transition:none !important;font-family:jaf-facitweb, Helvetica, Arial;font-weight:normal;line-height:24px;transform-origin:top left}label+.inputField{margin-top:16px;position:relative;width:100%;color:rgba(0, 0, 0, 0.87);cursor:text;display:inline-flex;font-size:0.875rem;box-sizing:border-box;align-items:center;font-family:jaf-facitweb, Helvetica, Arial;font-weight:400;line-height:1.1876em}label+.inputField:before{left:0;right:0;border:1px solid #D4D4E0;bottom:0;height:48px;content:\"\\00a0\";padding:0px 16px;position:absolute;transition:none;border-bottom:1px solid #D4D4E0;border-radius:8px;pointer-events:none}input{-webkit-box-shadow:0 0 0 100px #fff inset!important;-webkit-text-fill-color:#000;animation-name:mui-auto-fill;animation-duration:5000s;font:inherit;width:100%;border:0;height:20px;margin:0;display:block;padding:14px 16px;font-size:14px;min-width:0;background:none;box-sizing:content-box;font-style:normal;font-family:jaf-facitweb, Helvetica, Arial;font-weight:400;line-height:20px;letter-spacing:inherit;-webkit-tap-highlight-color:transparent}input:focus{outline:0}label+.inputField:after{left:0;right:0;border:1px solid #0F5FFF;bottom:0;height:48px;content:\"\";padding:0px 16px;position:absolute;transform:scaleX(0);transition:none;border-bottom:1px solid #0F5FFF;border-radius:8px;pointer-events:none}.parent_recovery_links{font-style:normal;font-family:jaf-facitweb, Helvetica, Arial;font-weight:normal;line-height:20px;margin-bottom:20px;text-align:center;display:block}.parent_recovery_links .recovery_links .markdownText{color:#0078FF}a:link,a:visited,a:hover,a:active{text-decoration:none}";
+
+const LoginForm = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+    this.loginShouldOccur = createEvent(this, "loginShouldOccur", 7);
+    /**
+     * State: _errorMessage: string
+     * Error message to display if login fails
+     */
+    this._errorMessage = "";
+    /**
+     * State: showError: string
+     * Controller for showing error message
+     */
+    this.showError = false;
+  }
+  login() {
+    let form = this.host.querySelector('form');
+    if (form.reportValidity()) {
+      let inputs = this.host.querySelectorAll('input');
+      this.loginShouldOccur.emit({ username: inputs[0].value, password: inputs[1].value });
+    }
+  }
+  displayError(event) {
+    console.log(event);
+    this._errorMessage = event.detail.text;
+    this.showError = !event.detail.show;
+  }
+  handleEnter() {
+    this.login();
+  }
+  render() {
+    return (h("div", { id: "loginFormContainer" }, h("form", { class: "loginForm" }, h("div", { class: "loginFormFields" }, h("div", { class: "inputFieldContainer" }, h("label", null, "Policyholder Email ", h("span", { class: "req" }, "*")), h("div", { class: "inputField" }, h("input", { name: "username", required: true }))), h("div", { class: "inputFieldContainer" }, h("label", null, "Password ", h("span", { class: "req" }, "*")), h("div", { class: "inputField" }, h("input", { name: "password", type: "password", required: true }))), h("error-message", { hidden: this.showError, errorMessage: this._errorMessage }), h("p", { class: "parent_recovery_links" }, h("a", { class: "recovery_links", href: "/myaccount/passwordreset/", target: "_blank" }, h("div", { class: "markdownWrapper" }, h("span", { class: "markdownParagraph" }, h("span", { class: "markdownText" }, "Forgotten Password"))))), h("p", { class: "parent_recovery_links" }, h("a", { class: "recovery_links", href: "/myaccount/login/help-menu", target: "_blank" }, h("div", { class: "markdownWrapper" }, h("span", { class: "markdownParagraph" }, h("span", { class: "markdownText" }, "Need help logging in?"))))), h("button", { class: "submitButton", type: "button", onClick: () => { this.login(); } }, h("span", { class: "buttonLabel" }, "Login"))))));
+  }
+  get host() { return getElement(this); }
+};
+LoginForm.style = loginFormCss;
+
+export { LoginForm as login_form };
